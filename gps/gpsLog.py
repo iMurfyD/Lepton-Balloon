4efd5fdc0795eaaecd472f3445af221a64749f47
Avery <averybod@gmail.com>
2017-04-26 15:39:20 +0000

    restructured gps to only require single logfile

--- gps/gpsLog.py
+++ gps/gpsLog.py
@@ -12,9 +12,6 @@ try:
         if line[0:13] == "GpsPrimaryLog":
             log1 = line[14:-1]
             #print log1
-        elif line[0:15] == "GpsSecondaryLog":
-            log2 = line[16:-1]
-            #print log2
 except IOError:
     print "Could not open Config File"
     ser.close()
@@ -23,24 +20,22 @@ except IOError:
 # close config file
 cfgFile.close()    
 
-# check if logfiles have been discovered in cfg file
-if (not 'log1' in locals() or not 'log2' in locals()):
-    print "Gps logfiles not found in cfg file"
+# check if logfile has been discovered in cfg file
+if (not 'log1' in locals()):
+    print "Gps logfile not found in cfg file"
     ser.close()
     sys.exit()
 
-# try to open logfiles for append + read
+# try to open logfile for append + read
 try:
     log1File = open(log1,'a+')
-    log2File = open(log2,'a+')
 except IOError:
-    print "Could not open log files"
+    print "Could not open gps log file"
     ser.close()
     sys.exit()
 
-# close logfiles
+# close logfile
 log1File.close()
-log2File.close()
 
 while 1:
     try:
@@ -49,20 +44,15 @@ while 1:
             # try to open log file 1
             try:
                 logFile = open(log1,'a+')
-                print "logfile 1 open"
             except IOError:
-                # try to open log file 2
-                try:
-                    logFile = open(log2,'a+')
-                except IOError:
-                    print "Uh oh..."
+                print "Uh oh..."
             # parse nmea data    
             msg = pynmea2.parse(data)
             # get lat lon alt and time
-            print msg.lat
-            print msg.lon
-            print msg.altitude
-            print msg.timestamp
+            #print msg.lat
+            #print msg.lon
+            #print msg.altitude
+            #print msg.timestamp
             # concatenate into string
             data = msg.timestamp.strftime("%H:%M:%S") + ',' + str(msg.lat) + ',' + str(msg.lon) + ',' + str(msg.altitude) + '\n'
             # write data to file
