2299b31a707de015ceeb45667b4cbed2d2459566
Avery <averybod@gmail.com>
2017-04-27 00:36:15 +0000

    added configurable oversampling for ms5637

--- ms5637/ms5637_Log.py
+++ ms5637/ms5637_Log.py
@@ -1,8 +1,5 @@
-# Distributed with a free-will license.
-# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
-# MS5637_02BA03
-# This code is designed to work with the MS5637-02BA03_I2CS I2C Mini Module available from ControlEverything.com.
-# https://www.controleverything.com/content/Analog-Digital-Converters?sku=MS5637-02BA03_I2CS#tabs-0-product_tabset-2
+# modified code from controleverything.com
+# Avery Bodenstein
 
 import smbus
 import time
@@ -14,6 +11,9 @@ try:
     for line in cfgFile:
         if line[0:9] == "MS5637Log":
             log = line[10:-1]
+        elif line[0:9] == "MS5637OSR":
+            OSR = line[10:-1]
+            OSR = int(OSR)
 except IOError:
     print "Could not open config file"
     sys.exit()
@@ -35,6 +35,36 @@ except IOError:
 
 # close logfile
 logFile.close()
+
+# check if OSR is valid
+if (not 'OSR' in locals()):
+    print "no OSR defined in logfile"
+    print "defaulting to 256"
+    pressureCmd = 0x40
+    tempCmd = 0x50
+elif OSR == 256:
+    pressureCmd = 0x40
+    tempCmd = 0x50
+elif OSR == 512:
+    pressureCmd = 0x42
+    tempCmd = 0x52
+elif OSR == 1024:
+    pressureCmd = 0x44
+    tempCmd = 0x54
+elif OSR == 2048:
+    pressureCmd = 0x46
+    tempCmd = 0x56
+elif OSR == 4096:
+    pressureCmd = 0x48
+    tempCmd = 0x58
+elif OSR == 8192:
+    pressureCmd = 0x4A
+    tempCmd = 0x5A
+else:
+    print "OSR not valid"
+    print OSR
+    pressureCmd = 0x40
+    tempCmd = 0x50
 
 # Get I2C bus
 bus = smbus.SMBus(1)
@@ -73,8 +103,8 @@ C6 = data[0] * 256 + data[1]
 while 1:
     try:
         # MS5637_02BA03 address, 0x76(118)
-        #		0x40(64)	Pressure conversion(OSR = 256) command
-        bus.write_byte(0x76, 0x40)
+        # begin pressure conversion using OSR defined in cfg file
+        bus.write_byte(0x76, pressureCmd)
 
         time.sleep(0.5)
 
@@ -85,8 +115,8 @@ while 1:
         D1 = value[0] * 65536 + value[1] * 256 + value[2]
 
         # MS5637_02BA03 address, 0x76(118)
-        #		0x50(64)	Temperature conversion(OSR = 256) command
-        bus.write_byte(0x76, 0x50)
+        # begin temperature conversion using OSR defined in cfg file
+        bus.write_byte(0x76, tempCmd)
 
         time.sleep(0.5)
 
@@ -96,6 +126,7 @@ while 1:
         value = bus.read_i2c_block_data(0x76, 0x00, 3)
         D2 = value[0] * 65536 + value[1] * 256 + value[2]
 
+        # convert raw temp value to actual temperature
         dT = D2 - C5 * 256
         TEMP = 2000 + dT * C6 / 8388608
         OFF = C2 * 131072 + (C4 * dT) / 64
@@ -104,6 +135,7 @@ while 1:
         OFF2 = 0
         SENS2 = 0
 
+        # compensate for nonlinearities at low temperature
         if TEMP > 2000 :
             T2 = 5 * dT * dT / 274877906944
             OFF2 = 0
@@ -116,12 +148,13 @@ while 1:
                 OFF2 = OFF2 + 17 * ((Temp + 1500) * (Temp + 1500))
                 SENS2 = SENS2 + 9 * ((Temp + 1500) * (Temp +1500))
 
+        # convert raw pressure value to actual pressure
         TEMP = TEMP - T2
         OFF = OFF - OFF2
         SENS = SENS - SENS2
         pressure = ((((D1 * SENS) / 2097152) - OFF) / 32768.0) / 100.0
         cTemp = TEMP / 100.0
-        fTemp = cTemp * 1.8 + 32
+
         # try to open logfile
         try:
             logFile = open(log,'a')
@@ -135,7 +168,6 @@ while 1:
         logFile.close()
         print "Pressure : %.2f mbar" %pressure
         print "Temperature in Celsius : %.2f C" %cTemp
-        print "Temperature in Fahrenheit : %.2f F" %fTemp
     except KeyboardInterrupt:
         logFile.close()
         sys.exit()
