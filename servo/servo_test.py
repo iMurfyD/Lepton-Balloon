c6484a895a1e413a595adc2e17cc71ed0629b1d6
Avery <averybod@gmail.com>
2017-04-28 20:26:21 +0000

    fixed pin and max angles in servo_test.py

--- servo/servo_test.py
+++ servo/servo_test.py
@@ -6,7 +6,8 @@ import wiringpi
 wiringpi.wiringPiSetupGpio()
 
 # set pin 18 to be a PWM output
-wiringpi.pinMode(17, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pinMode(17, wiringpi.GPIO.INPUT)
 
 # set the PWM mode to milliseconds type
 wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
@@ -17,14 +18,14 @@ wiringpi.pwmSetRange(2000)
 
 delay_period = 0.01
 
-wiringpi.pwmWrite(17,120)
+wiringpi.pwmWrite(18,120)
 time.sleep(1)
 
 while True:
-	for pulse in range(50, 250, 1):
-		wiringpi.pwmWrite(17,pulse)
+	for pulse in range(100, 200, 1):
+		wiringpi.pwmWrite(18,pulse)
 		time.sleep(delay_period)
-	for pulse in range(250, 50, -1):
-		wiringpi.pwmWrite(17,pulse)
+	for pulse in range(200, 100, -1):
+		wiringpi.pwmWrite(18,pulse)
 		time.sleep(delay_period)
 
