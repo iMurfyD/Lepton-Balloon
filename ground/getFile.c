336236efa8e5555993b0c9a58cb83cd6dbd33ced
Avery Bodenstein <averybod@gmail.com>
2017-05-15 20:11:10 +0000

    preliminary groundside radio driver

---
+++ ground/getFile.c
@@ -0,0 +1,96 @@
+// Transfers a binary file to arduino over I2C for downlink
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include "../downlink/gpio.h"
+
+#define ADDR 0x0F
+#define BUSYPIN 27
+#define RSTPIN 22
+#define CHUNK_SIZE 16
+
+int main(int argc, char **argv){
+  uint8_t buf[CHUNK_SIZE];
+  char *outFilename = NULL;
+  char i2cFilename[20];
+  int I2C,outFile;
+  int index;
+  int c;
+  int nBytes;
+  int end = 0;
+
+  opterr = 0;
+  // parse arguments
+  while ((c = getopt (argc, argv, "o:")) != -1)
+    switch (c)
+      {
+      case 'o':
+        outFilename = optarg;
+        break;
+      case '?':
+        if (optopt == 'o')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+  // check if input file was specified
+  if(outFilename == NULL){
+    printf("Output file required\n");
+    return 0;
+  }
+  // print file name for debugging
+  printf ("outFilename = %s\n",outFilename);
+
+  // open input file
+  outFile = open(outFilename,O_WRONLY|O_CREAT);
+  if (outFile<0){
+    printf("Failed to open output file\n");
+    return 0;
+  }
+  // open i2c interface
+  snprintf(i2cFilename,19,"/dev/i2c-%d",1);
+  I2C = open(i2cFilename, O_RDWR);
+  if (I2C<0){
+    printf("Failed to initialize I2C Bus\n");
+    // close input file
+    close(outFile);
+    return 0;
+  }
+
+  // get gpio
+  gpioExport(BUSYPIN);
+  gpioExport(RSTPIN);
+  // set direction
+  gpioDirection(BUSYPIN,GPIO_IN);
+  gpioDirection(RSTPIN,GPIO_OUT);
+  gpioSet(RSTPIN,1);
+  printf("Got GPIO\n");
+
+  // set device address
+  if (ioctl(I2C, I2C_SLAVE, ADDR) < 0){
+    // could not set device as slave
+    printf("Could not find device\n");
+    return 0;
+  }
+
+  // unregister gpios
+  gpioUnexport(BUSYPIN);
+  gpioUnexport(RSTPIN);
+  // close i2c interface
+  close(I2C);
+  // close input file
+  close(outFile);
+}
