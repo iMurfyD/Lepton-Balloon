ac5bd0feae462d63fb77518484531532aef731aa
Avery Bodenstein <averybod@gmail.com>
2017-05-15 21:04:02 +0000

    Correct file size, bad md5. No gpio

--- ground/getFile.c
+++ ground/getFile.c
@@ -13,16 +13,21 @@
 #define ADDR 0x0F
 #define BUSYPIN 27
 #define RSTPIN 22
-#define CHUNK_SIZE 16
+//#define CHUNK_SIZE 16
+#define BUFSIZE 1024
+
+// commands
+#define READ 0
+#define PEEK 1
+#define CHECKEOF 2
 
 int main(int argc, char **argv){
-  uint8_t buf[CHUNK_SIZE];
+  //uint8_t buf[CHUNK_SIZE];
   char *outFilename = NULL;
   char i2cFilename[20];
   int I2C,outFile;
   int index;
   int c;
-  int nBytes;
   int end = 0;
 
   opterr = 0;
@@ -55,7 +60,7 @@ int main(int argc, char **argv){
   printf ("outFilename = %s\n",outFilename);
 
   // open input file
-  outFile = open(outFilename,O_WRONLY|O_CREAT);
+  outFile = open(outFilename,O_WRONLY|O_CREAT|O_TRUNC);
   if (outFile<0){
     printf("Failed to open output file\n");
     return 0;
@@ -84,6 +89,46 @@ int main(int argc, char **argv){
     // could not set device as slave
     printf("Could not find device\n");
     return 0;
+  }
+
+  uint8_t command[3];
+  uint8_t data[2];
+  uint8_t dataBuf[BUFSIZE];
+  uint16_t nBytes = 0;
+  uint8_t newFile = 0;
+  // iterate until EOF
+  while(!newFile){
+   // check available data
+   command[0]= PEEK;
+   write(I2C,command,1);
+   read(I2C,data,2);
+   nBytes = (data[0] << 8) | data[1];
+   // if there is data
+   if(nBytes>0){
+     printf("got %d bytes\n",nBytes);
+     // write read data command
+     command[0] = READ;
+     command[1] = data[0];
+     command[2] = data[1];
+     write(I2C,command,3);
+     // read data
+     nBytes = read(I2C,dataBuf,nBytes);
+     // write data to file 
+     write(outFile,dataBuf,nBytes);
+   }
+   // check if EOF
+   else{
+     command[0] = CHECKEOF;
+     write(I2C,command,1);
+     read(I2C,data,1);
+     if(data[0]==1){
+       // raise EOF flag
+       newFile = 1;
+       printf("New File!\n");
+     }
+   }
+   // prevent loop from becoming overly fast
+   usleep(5000);
   }
 
   // unregister gpios
