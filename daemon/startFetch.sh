ca9d656faab0410a8366fdfcf29d57024f98d8dc
Avery <averybod@gmail.com>
2017-05-29 15:01:00 +0000

    working text file fetch

--- daemon/startFetch.sh
+++ daemon/startFetch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 service gpsFetchD start
 service visCamFetchD start
+service msFetchD start
--- daemon/stopFetch.sh
+++ daemon/stopFetch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 service gpsFetchD stop
 service visCamFetchD stop
+service msFetchD stop
